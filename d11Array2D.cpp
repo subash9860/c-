766b322e7e57d00a32fa97907d5fb32e737bbbc2
subash9860 <subaazkc@gmail.com>
2021-03-26 01:05:53 +0545

    day 11 2D Array

---
+++ d11Array2D.cpp
@@ -0,0 +1,110 @@
+/*
+Context
+Given a  2D Array, :
+
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+We define an hourglass in  to be a subset of values with indices falling in this pattern in 's graphical representation:
+
+a b c
+  d
+e f g
+There are  hourglasses in , and an hourglass sum is the sum of an hourglass' values.
+
+Task
+Calculate the hourglass sum for every hourglass in , then print the maximum hourglass sum.
+
+Example
+
+In the array shown above, the maximum hourglass sum is  for the hourglass in the top left corner.
+
+Input Format
+
+There are  lines of input, where each line contains  space-separated integers that describe the 2D Array .
+
+Constraints
+
+Output Format
+
+Print the maximum hourglass sum in .
+
+Sample Input
+
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 2 4 4 0
+0 0 0 2 0 0
+0 0 1 2 4 0
+Sample Output
+
+19
+Explanation
+
+ contains the following hourglasses:
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  1       0       0       0
+1 1 1   1 1 0   1 0 0   0 0 0
+
+0 1 0   1 0 0   0 0 0   0 0 0
+  1       1       0       0
+0 0 2   0 2 4   2 4 4   4 4 0
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  0       2       4       4
+0 0 0   0 0 2   0 2 0   2 0 0
+
+0 0 2   0 2 4   2 4 4   4 4 0
+  0       0       2       0
+0 0 1   0 1 2   1 2 4   2 4 0
+The hourglass with the maximum sum () is:
+
+2 4 4
+  2
+1 2 4
+
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    vector<vector<int>> arr(6);
+    for (int i = 0; i < 6; i++) {
+        arr[i].resize(6);
+
+        for (int j = 0; j < 6; j++) {
+            cin >> arr[i][j];
+        }
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+    
+    int maxsum=-64;  
+    int hoursum;   
+    for(int i=0;i<4;i++)
+    {
+        for(int j=0;j<4;j++)
+        {
+            hoursum=arr[i+1][j+1];    
+            for(int k=0;k<3;k++)
+                {                   
+                    hoursum = hoursum + arr[i][j+k] + arr[i+2][j+k]; 
+                }             
+            if(hoursum > maxsum)         
+                maxsum = hoursum;       
+        }      
+    }     
+    cout<<maxsum;
+    
+    return 0;
+}
