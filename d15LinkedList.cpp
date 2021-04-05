70c0db43c9b45df087147573c1e17ab317c9248b
subash9860 <subaazkc@gmail.com>
2021-04-05 23:00:29 +0545

    Day 15 Linked List

---
+++ d15LinkedList.cpp
@@ -0,0 +1,89 @@
+/*
+Task
+Complete the insert function in your editor so that it creates a new Node (pass  as the Node constructor argument) and inserts it at the tail of the linked list referenced by the  parameter. Once the new node is added, return the reference to the  node.
+
+Note: The  argument is null for an empty list.
+
+Input Format
+
+The first line contains T, the number of elements to insert.
+Each of the next  lines contains an integer to insert at the end of the list.
+
+Output Format
+
+Return a reference to the  node of the linked list.
+
+Sample Input
+
+STDIN   Function
+-----   --------
+4       T = 4
+2       first data = 2
+3
+4
+1       fourth data = 1
+Sample Output
+
+2 3 4 1
+Explanation
+
+, so your method will insert  nodes into an initially empty list.
+First the code returns a new node that contains the data value  as the  of the list. Then create and insert nodes , , and  at the tail of the list.
+
+*/
+
+#include <iostream>
+#include <cstddef>
+using namespace std;	
+class Node
+{
+    public:
+        int data;
+        Node *next;
+        Node(int d){
+            data=d;
+            next=NULL;
+        }
+};
+class Solution{
+    public:
+
+      Node* insert(Node *head,int data)
+      {
+          //Complete this method
+          Node * newNode = new Node(data);
+          Node * tail = head;
+          if ( !head ) { return newNode; }
+          
+          // Locate tail node
+          for (;tail->next; tail = tail->next);
+
+          // Insert at end of list
+          tail->next = newNode;
+          
+          return head;
+      }
+
+      void display(Node *head)
+      {
+          Node *start=head;
+          while(start)
+          {
+              cout<<start->data<<" ";
+              start=start->next;
+          }
+      }
+};
+int main()
+{
+	Node* head=NULL;
+  	Solution mylist;
+    int T,data;
+    cin>>T;
+    while(T-->0){
+        cin>>data;
+        head=mylist.insert(head,data);
+    }	
+	mylist.display(head);
+		
+}
